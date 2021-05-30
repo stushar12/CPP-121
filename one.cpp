b087b525e4c775fdd9df23e94aa433f4311668dc
Tushar Saxena <stushar345@gmail.com>
2021-05-31 00:31:03 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -1,4 +1,4 @@
-vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
         {
             vector<int> v;
             int i=0,j=0,k=0;
@@ -6,10 +6,7 @@ vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
             {
                 if(A[i]==B[j] && B[j]==C[k])
                 {
-                    if(find(v.begin(),v.end(),A[i])==v.end())
-                {
-                    v.emplace_back(A[i]);
-                }
+                     v.push_back(A[i]);
                     i++;
                     j++;
                     k++;
@@ -26,6 +23,16 @@ vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
                 {
                     k++;
                 }
-            }
+                
+                int xx=A[i-1];
+                int yy=B[j-1];
+                int zz=C[k-1];
+                while(i<n1 and xx==A[i])i++;                    //if there are repeating elements then just skip those elements
+                while(j<n2 and yy==B[j])j++;                     //eg. A[]={3,3,3}       
+                while(k<n3 and zz==C[k])k++;                  //       B[]={3,3,3}
+                                                                //     C[]={3,3,3}
+                  }
+            if(v.size()==0)
+            v.push_back(-1);
             return v;
         }
